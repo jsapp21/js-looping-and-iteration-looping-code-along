5fd9abb263693c5777a676fc921dba6890910fc7
James Sapp <jameseisapp@gmail.com>
2021-02-01 11:18:52 -0600

    commit tree 442bbca2321151e8a8bbdde01a9d4af8a36ead8c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const myArr = ['Lisa', 'Kaitlin', 'Jan']
+const event = ["surprise"]
+
+function writeCards(myArr, event) {
+    let thankYouCards = []
+    for (let i = 0; i < myArr.length; i++) {
+        thankYouCards.push(`Thank you, ${myArr[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+
+
+function countDown(number) {
+    let i = 0;
+    while(i < number) {
+        console.log(number);
+        number -= 1;
+    }
+    // returns last executed element
+    console.log(number);
+}
+
